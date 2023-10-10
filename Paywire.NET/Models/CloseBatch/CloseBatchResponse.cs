302e86a7712f241ce2cc5310667541df15253f42
srs-binnys <binny.samuel@spirerecoverysolutions.com>
2023-10-10 21:03:10 +0000

    Implemented Close Batch Transaction Type

---
+++ Paywire.NET/Models/CloseBatch/CloseBatchResponse.cs
@@ -0,0 +1,24 @@
+﻿using Paywire.NET.Models.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Paywire.NET.Models.CloseBatch
+{
+    [XmlRoot("PAYMENTRESPONSE")]
+    public class CloseBatchResponse : BasePaywireResponse
+    {
+        public string BATCHID { get; set; }
+        public string SALESTOTAL { get; set; }
+        public string SALESRECS { get; set; }
+        public string CREDITAMT { get; set; }
+        public string CREDITRECS { get; set; }
+        public string NETCRAMT { get; set; }
+        public string NETCRRECS { get; set; }
+        public string VOIDAMT { get; set; }
+        public string VOIDRECS { get; set;}
+    }
+}
